5377224ed025ccf88dc37dc0d60363a3a5ca350e
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-02-21 17:47:13 +0200

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,17 @@
-// import { test, expect } from '@jest/globals';
-
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
+import path from 'path';
 import genDiff from '../index.js';
 
-// const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const shallowJson1 = '__fixtures__/shallow1.json';
-const shallowJson2 = '__fixtures__/shallow2.json';
-const shallowDiff = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`
+const shallowJson1 = getFixturePath('shallow1.json');
+const shallowJson2 = getFixturePath('shallow2.json');
+const shallowDiff = readFileSync(getFixturePath('shallow-diff.txt'), 'utf-8');
 
 test('shallow diff', () => {
   expect(genDiff(shallowJson1, shallowJson2)).toEqual(shallowDiff);
-})
+});
