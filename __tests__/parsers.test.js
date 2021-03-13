d12a3464433ae56605ed9199b7e6d67e9a9e92cb
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-13 08:49:31 +0200

    Added test for parsers.js

---
+++ __tests__/parsers.test.js
@@ -0,0 +1,28 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import parseData from '../src/parsers.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+
+const jsonFile = getFixturePath('deep1.json');
+const yamlFile = getFixturePath('deep1.yml');
+const jsonData = parseData(jsonFile);
+const yamlData = parseData(yamlFile);
+
+test('parsed data should be object', () => {
+  expect(typeof jsonData).toBe('object');
+});
+test('parsed data should not be null', () => {
+  expect(jsonData).not.toBeNull();
+});
+test('parsed JSON should be equal to parsed YAML', () => {
+  expect(jsonData).toEqual(yamlData);
+});
+test('only JSON and YAML files can be parsed', () => {
+  const diffFile = getFixturePath('stylish-diff.txt');
+  expect(() => {
+    parseData(diffFile);
+  }).toThrow();
+});
