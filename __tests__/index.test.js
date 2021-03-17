aaa0e91b622ea35d41d633fbf5b32052a4a93d3a
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-17 21:40:39 +0200

    Refactored tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,31 +7,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-const json1 = getFixturePath('deep1.json');
-const json2 = getFixturePath('deep2.json');
-const yaml1 = getFixturePath('deep1.yml');
-const yaml2 = getFixturePath('deep2.yaml');
+const stylishExample = readFileSync(getFixturePath('diffs/stylish-diff.txt'), 'utf-8');
+const plainExample = readFileSync(getFixturePath('diffs/plain-diff.txt'), 'utf-8');
+const jsonExample = readFileSync(getFixturePath('diffs/json-diff.txt'), 'utf-8');
+const table = [
+  ['stylish', stylishExample],
+  ['plain', plainExample],
+  ['json', jsonExample],
+];
 
-test('should match stylish example', () => {
-  const stylishFixture = getFixturePath('stylish-diff.txt');
-  const stylishFixtureData = readFileSync(stylishFixture, 'utf-8');
+describe('json diff should work', () => {
+  const initialData = getFixturePath('data/deep1.json');
+  const updatedData = getFixturePath('data/deep2.json');
 
-  expect(genDiff(json1, json2, 'stylish')).toEqual(stylishFixtureData);
+  test.each(table)('%s output', (format, example) => {
+    expect(genDiff(initialData, updatedData, format)).toBe(example);
+  });
 });
 
-test('should match plain example', () => {
-  const plainFixture = getFixturePath('plain-diff.txt');
-  const plainFixtureData = readFileSync(plainFixture, 'utf-8');
+describe('yaml diff should work', () => {
+  const initialData = getFixturePath('data/deep1.yml');
+  const updatedData = getFixturePath('data/deep2.yaml');
 
-  expect(genDiff(json1, json2, 'plain')).toEqual(plainFixtureData);
-});
-
-test('json and yaml diff should be equal', () => {
-  const jsonStylishDiff = genDiff(json1, json2, 'stylish');
-  const yamlStylishDiff = genDiff(yaml1, yaml2, 'stylish');
-  const jsonPlainDiff = genDiff(json1, json2, 'plain');
-  const yamlPlainDiff = genDiff(yaml1, yaml2, 'plain');
-
-  expect(jsonStylishDiff).toEqual(yamlStylishDiff);
-  expect(jsonPlainDiff).toEqual(yamlPlainDiff);
+  test.each(table)('%s output', (format, example) => {
+    expect(genDiff(initialData, updatedData, format)).toBe(example);
+  });
 });
