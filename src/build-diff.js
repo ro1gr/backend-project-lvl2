a11bb7986f13bc0628657f3ca6594f958acfc039
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-17 15:01:24 +0200

    Fixed build-diff.js working only with filepaths

--- src/build-diff.js
+++ src/build-diff.js
@@ -1,21 +1,17 @@
 import _ from 'lodash';
-import parseData from './parsers.js';
 import computeState from './compute-state.js';
 import computeValue from './compute-value.js';
 
-export default (filepath1, filepath2) => {
-  const data1 = parseData(filepath1);
-  const data2 = parseData(filepath2);
-
-  const iter = (initialData, updatedData) => {
-    const initialKeys = Object.keys(initialData);
-    const updatedKeys = Object.keys(updatedData);
+export default (initialData, updatedData) => {
+  const iter = (currentInitialData, currentUpdatedData) => {
+    const initialKeys = Object.keys(currentInitialData);
+    const updatedKeys = Object.keys(currentUpdatedData);
     const mergedKeys = _.union(initialKeys, updatedKeys);
     const sortedKeys = [...mergedKeys].sort();
 
     return sortedKeys.map((key) => {
-      const initialValue = initialData[key];
-      const updatedValue = updatedData[key];
+      const initialValue = currentInitialData[key];
+      const updatedValue = currentUpdatedData[key];
       const entry = { key };
       if (_.isObject(initialValue) && _.isObject(updatedValue)) {
         entry.children = iter(initialValue, updatedValue);
@@ -27,5 +23,5 @@ export default (filepath1, filepath2) => {
     });
   };
 
-  return iter(data1, data2);
+  return iter(initialData, updatedData);
 };
