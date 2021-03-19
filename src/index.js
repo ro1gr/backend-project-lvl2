012db0041e74e4df832bc8a9f168a519bde17118
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-19 17:36:15 +0200

    Changed constant names

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import buildDiff from './build-diff.js';
 import formatDiff from './format-diff.js';
 
 export default (filepath1, filepath2, format = 'stylish') => {
-  const initialData = parseData(filepath1);
-  const updatedData = parseData(filepath2);
-  const diff = buildDiff(initialData, updatedData);
+  const data1 = parseData(filepath1);
+  const data2 = parseData(filepath2);
+  const diff = buildDiff(data1, data2);
   const formattedDiff = formatDiff(diff, format);
 
   return formattedDiff;
