b15181272177dd555898e0b940608af3c6d44f5a
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-19 17:40:49 +0200

    Refactored buildDiff

--- src/build-diff.js
+++ src/build-diff.js
@@ -2,26 +2,22 @@ import _ from 'lodash';
 import computeState from './compute-state.js';
 import computeValue from './compute-value.js';
 
-export default (initialData, updatedData) => {
-  const iter = (currentInitialData, currentUpdatedData) => {
-    const initialKeys = Object.keys(currentInitialData);
-    const updatedKeys = Object.keys(currentUpdatedData);
-    const mergedKeys = _.union(initialKeys, updatedKeys);
-    const sortedKeys = _.sortBy(mergedKeys);
+const buildDiff = (data1, data2) => {
+  const mergedKeys = _.union(Object.keys(data1), Object.keys(data2));
+  const sortedKeys = _.sortBy(mergedKeys);
 
-    return sortedKeys.map((key) => {
-      const initialValue = currentInitialData[key];
-      const updatedValue = currentUpdatedData[key];
-      const entry = { key };
-      if (_.isObject(initialValue) && _.isObject(updatedValue)) {
-        const children = iter(initialValue, updatedValue);
-        return { ...entry, children };
-      }
-      const state = computeState(initialValue, updatedValue);
-      const value = computeValue(state, initialValue, updatedValue);
-      return { ...entry, state, value };
-    });
-  };
-
-  return iter(initialData, updatedData);
+  return sortedKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    const entry = { key };
+    if (_.isObject(value1) && _.isObject(value2)) {
+      const children = buildDiff(value1, value2);
+      return { ...entry, children };
+    }
+    const state = computeState(value1, value2);
+    const value = computeValue(state, value1, value2);
+    return { ...entry, state, value };
+  });
 };
+
+export default buildDiff;
