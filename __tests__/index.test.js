b27e9800e2a2ef099109a0e96700ea48df14f9cb
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-20 21:26:10 +0200

    Refactored tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,29 +8,30 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (fixture) => readFileSync(getFixturePath(fixture), 'utf-8');
 
+const jsonData1 = getFixturePath('file1.json');
+const jsonData2 = getFixturePath('file2.json');
+const yamlData1 = getFixturePath('file1.yml');
+const yamlData2 = getFixturePath('file2.yml');
 const expectedStylishResult = readFixture('stylish-diff.txt');
 const expectedPlainResult = readFixture('plain-diff.txt');
 const expectedJsonResult = readFixture('json-diff.txt');
 
-const runTests = (data1, data2) => {
-  const cases = [
-    ['stylish', expectedStylishResult],
-    ['plain', expectedPlainResult],
-    ['json', expectedJsonResult],
+const runTests = (expectedResult, outputFormat) => {
+  const inputFormats = [
+    ['json', jsonData1, jsonData2],
+    ['yaml', yamlData1, yamlData2],
   ];
-  test.each(cases)('%s output', (format, example) => {
-    expect(genDiff(data1, data2, format)).toBe(example);
+  test.each(inputFormats)('%s data', (inputFormat, data1, data2) => {
+    expect(genDiff(data1, data2, outputFormat)).toBe(expectedResult);
   });
 };
 
-describe('should work with JSON', () => {
-  const data1 = getFixturePath('file1.json');
-  const data2 = getFixturePath('file2.json');
-  runTests(data1, data2);
-});
+const outputFormats = [
+  ['stylish', expectedStylishResult],
+  ['plain', expectedPlainResult],
+  ['json', expectedJsonResult],
+];
 
-describe('should work with YAML', () => {
-  const data1 = getFixturePath('file1.yml');
-  const data2 = getFixturePath('file2.yml');
-  runTests(data1, data2);
+describe.each(outputFormats)('should match %s diff', (outputFormat, expectedResult) => {
+  runTests(expectedResult, outputFormat);
 });
