1cde7ecbabff9a7ab3d1e8a82cec5d8976fbbe89
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-20 12:42:35 +0200

    Refactored parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-export default (filepath) => {
-  const extension = path.extname(filepath);
-  switch (extension) {
-    case '.json':
-      return JSON.parse(fs.readFileSync(filepath, 'utf-8'));
-    case '.yml':
-      return yaml.load(fs.readFileSync(filepath, 'utf-8'));
-    default:
-      throw new Error(`${extension} is not supported. Only JSON and YAML files can be parsed.`);
+const readData = (input) => {
+  if (fs.existsSync(input)) {
+    return fs.readFileSync(input);
   }
+  return input;
+}
+
+export default (input) => {
+  const data = readData(input);
+  return yaml.load(data);
 };
