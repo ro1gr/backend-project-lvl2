b468035c49f5e0059922bb91aca9d54fa009b4cb
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-20 13:07:45 +0200

    Refactored buildDiff

--- src/build-diff.js
+++ src/build-diff.js
@@ -1,6 +1,22 @@
 import _ from 'lodash';
-import computeState from './compute-state.js';
-import computeValue from './compute-value.js';
+
+const fillEntry = (entry, value1, value2) => {
+  const data = {};
+  if (_.isEqual(value1, value2)) {
+    data.state = 'unchanged';
+    data.value = { unchanged: value1 };
+  } else if (value2 === undefined) {
+    data.state = 'removed';
+    data.value = { removed: value1 };
+  } else if (value1 === undefined) {
+    data.state = 'added';
+    data.value = { added: value2 };
+  } else {
+    data.state = 'updated';
+    data.value = { removed: value1, added: value2 };
+  }
+  return { ...entry, ...data };
+};
 
 const buildDiff = (data1, data2) => {
   const mergedKeys = _.union(Object.keys(data1), Object.keys(data2));
@@ -14,9 +30,7 @@ const buildDiff = (data1, data2) => {
       const children = buildDiff(value1, value2);
       return { ...entry, children };
     }
-    const state = computeState(value1, value2);
-    const value = computeValue(state, value1, value2);
-    return { ...entry, state, value };
+    return fillEntry(entry, value1, value2);
   });
 };
 
