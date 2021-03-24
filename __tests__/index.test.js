8f98c6cf17ed491e0018d7bbc0ba73a1393a8f25
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-24 10:18:09 +0200

    Updated tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,37 +1,55 @@
-import { join, dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { readFileSync } from 'fs';
 import genDiff from '../index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (fixture) => readFileSync(getFixturePath(fixture), 'utf-8');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const readFixture = (fixture) => fs.readFileSync(getFixturePath(fixture), 'utf-8');
+
+const isValidJSON = (data) => {
+  try {
+    JSON.parse(data);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
 
 const expectedStylishResult = readFixture('stylish-diff.txt');
 const expectedPlainResult = readFixture('plain-diff.txt');
 const expectedJsonResult = readFixture('json-diff.txt');
 
 const outputFormats = [
-  ['stylish', expectedStylishResult],
-  ['plain', expectedPlainResult],
-  ['json', expectedJsonResult],
+  ['stylish'],
+  ['plain'],
+  ['json'],
 ];
 
-const runTests = (expectedResult, outputFormat) => {
-  const jsonFile1 = getFixturePath('file1.json');
-  const jsonFile2 = getFixturePath('file2.json');
-  const yamlFile1 = getFixturePath('file1.yml');
-  const yamlFile2 = getFixturePath('file2.yml');
+describe.each(outputFormats)('should match expected %s diff', (outputFormat) => {
+  const expected = {
+    stylish: expectedStylishResult,
+    plain: expectedPlainResult,
+    json: expectedJsonResult,
+  };
   const inputFormats = [
-    ['json', jsonFile1, jsonFile2],
-    ['yaml', yamlFile1, yamlFile2],
+    ['json'],
+    ['yml'],
   ];
-  test.each(inputFormats)('%s data', (inputFormat, data1, data2) => {
-    expect(genDiff(data1, data2, outputFormat)).toBe(expectedResult);
+  test.each(inputFormats)('%s data', (inputFormat) => {
+    const path1 = getFixturePath(`file1.${inputFormat}`);
+    const path2 = getFixturePath(`file2.${inputFormat}`);
+    expect(genDiff(path1, path2, outputFormat)).toBe(expected[outputFormat]);
   });
-};
+});
 
-describe.each(outputFormats)('should match %s diff', (outputFormat, expectedResult) => {
-  runTests(expectedResult, outputFormat);
+describe('should return valid JSON', () => {
+  test('json diff', () => {
+    const path1 = getFixturePath('file1.json');
+    const path2 = getFixturePath('file2.json');
+    const jsonDiff = genDiff(path1, path2, 'json');
+    expect(isValidJSON(jsonDiff)).toBeTruthy();
+  });
 });
