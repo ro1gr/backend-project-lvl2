30f61c58401fce27131143410257705e1d110d37
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-03-24 10:16:01 +0200

    Updated diff tree structure

--- src/build-diff.js
+++ src/build-diff.js
@@ -1,31 +1,43 @@
 import _ from 'lodash';
 
-const fillEntry = (entry, value1, value2) => {
-  if (_.isEqual(value1, value2)) {
-    return { ...entry, state: 'unchanged', value: { unchanged: value1 } };
-  }
-  if (value2 === undefined) {
-    return { ...entry, state: 'removed', value: { removed: value1 } };
-  }
-  if (value1 === undefined) {
-    return { ...entry, state: 'added', value: { added: value2 } };
-  }
-  return { ...entry, state: 'updated', value: { removed: value1, added: value2 } };
-};
-
 const buildDiff = (data1, data2) => {
   const mergedKeys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(mergedKeys);
 
   return sortedKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    const entry = { key };
-    if (_.isObject(value1) && _.isObject(value2)) {
-      const children = buildDiff(value1, value2);
-      return { ...entry, children };
+    if (!_.has(data1, key)) {
+      return {
+        key,
+        state: 'added',
+        value2: data2[key],
+      };
     }
-    return fillEntry(entry, value1, value2);
+    if (!_.has(data2, key)) {
+      return {
+        key,
+        state: 'removed',
+        value1: data1[key],
+      };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return {
+        key,
+        children: buildDiff(data1[key], data2[key]),
+      };
+    }
+    if (data1[key] !== data2[key]) {
+      return {
+        key,
+        state: 'updated',
+        value1: data1[key],
+        value2: data2[key],
+      };
+    }
+    return {
+      key,
+      state: 'unchanged',
+      value1: data1[key],
+    };
   });
 };
 
