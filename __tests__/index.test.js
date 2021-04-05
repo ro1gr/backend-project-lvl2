6d699c7122c38c22c42b2d5b3d9fbd407ec88e7f
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-04-05 21:13:53 +0300

    Refactored tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,35 +9,16 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const readFixture = (fixture) => fs.readFileSync(getFixturePath(fixture), 'utf-8');
 
-const isValidJSON = (data) => {
-  try {
-    JSON.parse(data);
-    return true;
-  } catch (e) {
-    return false;
-  }
+const expected = {
+  stylish: readFixture('stylish-diff.txt'),
+  plain: readFixture('plain-diff.txt'),
+  json: readFixture('json-diff.txt'),
 };
 
-const expectedStylishResult = readFixture('stylish-diff.txt');
-const expectedPlainResult = readFixture('plain-diff.txt');
-const expectedJsonResult = readFixture('json-diff.txt');
-
-const outputFormats = [
-  ['stylish'],
-  ['plain'],
-  ['json'],
-];
+const outputFormats = ['stylish', 'plain', 'json'];
 
 describe.each(outputFormats)('should match expected %s diff', (outputFormat) => {
-  const expected = {
-    stylish: expectedStylishResult,
-    plain: expectedPlainResult,
-    json: expectedJsonResult,
-  };
-  const inputFormats = [
-    ['json'],
-    ['yml'],
-  ];
+  const inputFormats = ['json', 'yml'];
   test.each(inputFormats)('%s data', (inputFormat) => {
     const path1 = getFixturePath(`file1.${inputFormat}`);
     const path2 = getFixturePath(`file2.${inputFormat}`);
@@ -45,11 +26,11 @@ describe.each(outputFormats)('should match expected %s diff', (outputFormat) => 
   });
 });
 
-describe('should return valid JSON', () => {
-  test('json diff', () => {
+describe('json diff should be valid', () => {
+  test('is valid', () => {
     const path1 = getFixturePath('file1.json');
     const path2 = getFixturePath('file2.json');
     const jsonDiff = genDiff(path1, path2, 'json');
-    expect(isValidJSON(jsonDiff)).toBeTruthy();
+    expect(() => JSON.parse(jsonDiff)).not.toThrow();
   });
 });
