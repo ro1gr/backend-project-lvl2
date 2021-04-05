a636e508fbe743df161fe6a6e04bee3a0a12eebc
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-04-05 21:19:17 +0300

    Refactored index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
 import fs from 'fs';
+import path from 'path';
 import parseData from './parsers.js';
 import buildDiff from './build-diff.js';
-import formatDiff from './format-diff.js';
+import formatDiff from './formatters/format-diff.js';
 
-const getData = (data) => {
-  if (fs.existsSync(data)) {
-    return fs.readFileSync(data);
-  }
-  return data;
+const getFormat = (filepath) => {
+  const extension = path.extname(filepath);
+  const format = extension.slice(1);
+  return format;
 };
-const getFormat = (string) => {
-  try {
-    JSON.parse(string);
-    return 'json';
-  } catch {
-    return 'yaml';
-  }
+const getData = (filepath) => {
+  const content = fs.readFileSync(filepath);
+  const format = getFormat(filepath);
+  return parseData(content, format);
 };
-
 export default (filepath1, filepath2, format = 'stylish') => {
   const data1 = getData(filepath1);
   const data2 = getData(filepath2);
-  const format1 = getFormat(data1);
-  const format2 = getFormat(data2);
-  const diff = buildDiff(parseData(data1, format1), parseData(data2, format2));
+  const diff = buildDiff(data1, data2);
   const formattedDiff = formatDiff(diff, format);
-
   return formattedDiff;
 };
