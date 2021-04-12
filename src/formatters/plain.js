7830a6b21fcd12144aa30bb7e3a16afc7f7bb93d
Roman Grigoryan <roman1grigoryan@gmail.com>
2021-04-12 11:51:36 +0300

    Refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,21 +10,22 @@ const formatValue = (value) => {
   }
   return value;
 };
+const getPropertyName = (paths) => paths.join('.');
 
 export default (diff) => {
-  const iter = (currentValue, acc) => {
-    const lines = currentValue
+  const iter = (node, paths) => {
+    const lines = node
       .flatMap((entry) => {
-        const newAcc = [...acc, entry.key];
+        const newPaths = [...paths, entry.key];
         switch (entry.state) {
           case 'complexValue':
-            return `${iter(entry.children, newAcc)}`;
+            return iter(entry.children, newPaths);
           case 'removed':
-            return `Property '${newAcc.join('.')}' was removed`;
+            return `Property '${getPropertyName(newPaths)}' was removed`;
           case 'added':
-            return `Property '${newAcc.join('.')}' was added with value: ${formatValue(entry.value)}`;
+            return `Property '${getPropertyName(newPaths)}' was added with value: ${formatValue(entry.value)}`;
           case 'updated':
-            return `Property '${newAcc.join('.')}' was updated. From ${formatValue(entry.value1)} to ${formatValue(entry.value2)}`;
+            return `Property '${getPropertyName(newPaths)}' was updated. From ${formatValue(entry.value1)} to ${formatValue(entry.value2)}`;
           case 'unchanged':
             return [];
           default:
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,66 +1,50 @@
-const appendSign = (indentation, state) => {
-  switch (state) {
-    case 'added':
-      return indentation.slice(2).concat('+ ');
-    case 'removed':
-      return indentation.slice(2).concat('- ');
-    default:
-      return indentation;
+const space = ' ';
+const width = 4;
+
+const formatLineIndentation = (depth, sign = ' ') => space.repeat(depth * width).slice(2).concat(`${sign} `);
+const formatBracketIndentation = (depth) => space.repeat((depth - 1) * width);
+const formatLines = (depth, lines) => [
+  '{',
+  ...lines,
+  `${formatBracketIndentation(depth)}}`,
+].join('\n');
+const formatValue = (currentValue, depth) => {
+  if (typeof currentValue !== 'object') {
+    return currentValue;
   }
+  if (currentValue === null) {
+    return null;
+  }
+
+  const lines = Object.entries(currentValue)
+    .map(([key, val]) => `${formatLineIndentation(depth)}${key}: ${formatValue(val, depth + 1)}`);
+
+  return formatLines(depth, lines);
 };
 
 export default (diff) => {
-  const indentation = {
-    space: ' ',
-    width: 4,
-  };
-
-  const iterateObject = (currentValue, depth) => {
-    if (currentValue === null) {
-      return null;
-    }
-    if (typeof currentValue !== 'object') {
-      return currentValue.toString();
-    }
-
-    const indentationSize = depth * indentation.width;
-    const currentIndentation = indentation.space.repeat(indentationSize);
-    const bracketIndentation = indentation.space.repeat(indentationSize - indentation.width);
-
-    const lines = Object.entries(currentValue)
-      .map(([key, val]) => `${currentIndentation}${key}: ${iterateObject(val, depth + 1)}`);
-
-    return [
-      '{',
-      ...lines,
-      `${bracketIndentation}}`,
-    ].join('\n');
-  };
-
   const iterateArray = (currentValue, depth) => {
-    const indentationSize = depth * indentation.width;
-    const currentIndentation = indentation.space.repeat(indentationSize);
-    const bracketIndentation = indentation.space.repeat(indentationSize - indentation.width);
-
     const lines = currentValue.flatMap((entry) => {
       switch (entry.state) {
         case 'complexValue':
-          return `${currentIndentation}${entry.key}: ${iterateArray(entry.children, depth + 1)}`;
+          return `${formatLineIndentation(depth)}${entry.key}: ${iterateArray(entry.children, depth + 1)}`;
+        case 'removed':
+          return `${formatLineIndentation(depth, '-')}${entry.key}: ${formatValue(entry.value, depth + 1)}`;
+        case 'added':
+          return `${formatLineIndentation(depth, '+')}${entry.key}: ${formatValue(entry.value, depth + 1)}`;
         case 'updated':
           return [
-            `${appendSign(currentIndentation, 'removed')}${entry.key}: ${iterateObject(entry.value1, depth + 1)}`,
-            `${appendSign(currentIndentation, 'added')}${entry.key}: ${iterateObject(entry.value2, depth + 1)}`,
+            `${formatLineIndentation(depth, '-')}${entry.key}: ${formatValue(entry.value1, depth + 1)}`,
+            `${formatLineIndentation(depth, '+')}${entry.key}: ${formatValue(entry.value2, depth + 1)}`,
           ];
+        case 'unchanged':
+          return `${formatLineIndentation(depth)}${entry.key}: ${formatValue(entry.value, depth + 1)}`;
         default:
-          return `${appendSign(currentIndentation, entry.state)}${entry.key}: ${iterateObject(entry.value, depth + 1)}`;
+          throw new Error(`${entry.state} is not expected.`);
       }
     });
 
-    return [
-      '{',
-      ...lines,
-      `${bracketIndentation}}`,
-    ].join('\n');
+    return formatLines(depth, lines);
   };
 
   return iterateArray(diff, 1);
